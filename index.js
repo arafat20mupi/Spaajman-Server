280abb9d53a345eea74bbcf079141f35cb2cd723
Arafat islam <arafat.innotimes.net@gmail.com>
2024-09-01 09:22:15 +0600

    Server Connect & get Data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000
-
+const { ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors({
@@ -16,8 +16,9 @@ app.use(cors({
 app.use(express.json());
 
 
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4zngp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb+srv://Spaajman:rCP7LNlCZryXWaoP@cluster0.ykgi9mv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,18 +28,28 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
+const database = client.db("Spaajman").collection("service");
 async function run() {
     try {
-        const database = client.db("shop").collection("service");
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
         app.get('/shop', async (req, res) => {
             const services = await database.find().toArray();
-            res.send(services);
+            res.send(services)
         })
+        app.get('/shop/:id', async (req, res) => {
+            try {
+                
+            } catch (error) {
+                console.error('Error fetching service:', error);            
+            }
+        });
 
 
 
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
@@ -46,6 +57,7 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
